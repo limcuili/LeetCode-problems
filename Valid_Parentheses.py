9390c6e20bb3cb52f6b03ac67eb821de46dd2f70
limcuili <limcuili@gmail.com>
2021-05-30 18:34:51 +0100

    force handling of single parentheses edge case

---
+++ Valid_Parentheses.py
@@ -0,0 +1,59 @@
+""" Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+Test with: python -m unittest Valid_Parentheses.py
+"""
+
+import unittest
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parentheses_pairs = {")": "(", "}": "{", "]": "["}
+        stack = []
+        for character in s:
+            if character in parentheses_pairs:
+                try:
+                    top = stack.pop()
+                    if parentheses_pairs[character] != top:
+                        return False
+                except:
+                    return False
+            else:
+                stack.append(character)
+
+        return stack == []
+
+
+class Test(unittest.TestCase):
+    def setUp(self) -> None:
+        self.solution = Solution()
+
+    def test_single_parentheses(self):
+        s = '()'
+        answer = self.solution.isValid(s)
+        self.assertIs(answer, True)
+
+    def test_consecutive_parentheses(self):
+        s = '()[]{}'
+        answer = self.solution.isValid(s)
+        self.assertIs(answer, True)
+
+    def test_stack_parentheses(self):
+        s = '{[]}'
+        answer = self.solution.isValid(s)
+        self.assertIs(answer, True)
+
+    def test_wrong_parentheses(self):
+        s = '(]'
+        answer = self.solution.isValid(s)
+        self.assertIs(answer, False)
+
+    def test_swapped_position_parentheses(self):
+        s = '([)]'
+        answer = self.solution.isValid(s)
+        self.assertIs(answer, False)
+
+    def test_single_parentheses(self):
+        s = ']'
+        answer = self.solution.isValid(s)
+        self.assertIs(answer, False)
