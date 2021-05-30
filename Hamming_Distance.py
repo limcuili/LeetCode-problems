3c6b6db74c8f0b130cddc44898c50aeb76a5815f
limcuili <limcuili@gmail.com>
2021-05-30 19:28:45 +0100

    list comprehension solution for hamming distance

--- Hamming_Distance.py
+++ Hamming_Distance.py
@@ -7,8 +7,19 @@ import unittest
 
 
 class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
+    def hammingDistance1(self, x: int, y: int) -> int:
+        binary_x = "{:032b}".format(x)
+        binary_y = "{:032b}".format(y)
+        differences = [i for i, j in zip(binary_x, binary_y) if i != j]
+        return len(differences)
+
+    def hammingDistance2(self, x: int, y: int) -> int:
+        # TODO: implement with bitwise operations
         return None
+
+    def hammingDistance(self, x: int, y: int) -> int:
+        # hook in whichever of the above implementation you want.
+        return self.hammingDistance1(x, y)
 
 
 class Test(unittest.TestCase):
@@ -25,6 +36,13 @@ class Test(unittest.TestCase):
     def test_two_difference(self):
         x = 4
         y = 1
+        expected_answer = 2
+        answer = self.solution.hammingDistance(x, y)
+        self.assertEqual(expected_answer, answer)
+
+    def test_edge_case(self):
+        x = 4
+        y = 14
         expected_answer = 2
         answer = self.solution.hammingDistance(x, y)
         self.assertEqual(expected_answer, answer)
