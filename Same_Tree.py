4c3a9deb9284882301e9978bc3f461c4afcc47c9
limcuili <limcuili@gmail.com>
2021-06-06 18:02:57 +0100

    recursive solution for same tree

--- Same_Tree.py
+++ Same_Tree.py
@@ -32,7 +32,13 @@ class TreeFactory:
 
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        return None
+        if not p and not q:
+            return True
+        if not q or not p:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
 
 
 class Test(unittest.TestCase):
